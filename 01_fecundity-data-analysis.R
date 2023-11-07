9a91dc0f130c2201dd59f4c9ba744a718eac352f
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2023-11-07 15:30:36 -0800

    Script and directory updates; finished first pass at 2021-2023 data plotting

--- 01_fecundity-data-analysis.R
+++ 01_fecundity-data-analysis.R
@@ -1,10 +1,15 @@
 # Packages and functions --------------------------------------------------
 
-pkgs <- c("tidyverse", "readxl", "here", "httr", "jsonlite", "magrittr")
+pkgs <- c("tidyverse", "ggridges", "readxl", "writexl", "here", "httr", "jsonlite", "magrittr",
+          "ggExtra", "ggpmisc")
 #install.packages(pkgs)
 
 library(tidyverse); theme_set(theme_bw(base_size = 18))
+library(ggridges)
+library(ggpmisc)
+library(ggExtra)
 library(readxl)
+library(writexl)
 library(here)
 library(httr)
 library(jsonlite)
@@ -176,35 +181,158 @@ fec_data_sum  <- fec_data |>
     )
   )
 
+# Save full and summarized data into one excel file
+list(fec_data, fec_data_sum) |> 
+  map( # Fix up column names so they look nicer in Excel
+    ~ .x |> 
+      rename_with(
+        ~ .x |> 
+          str_replace_all("_", " ") |> 
+          str_to_title() |> 
+          str_replace_all("(?i)poh|yn|gr|eu|id", str_to_upper) |> # Capitalize all the acronyms
+          str_replace_all("(?i)mm", str_to_lower) # Convert millimeters (mm) to lowercase
+      )
+  ) |> 
+  set_names(c("Full data", "Summarized by F ID")) |> # Specify sheet names
+  write_xlsx(
+    path = here(
+      paste0(
+        "R-OUT_Fecundity-Data_all-years_", 
+        Sys.Date(), 
+        ".xlsx"
+      )
+    )
+  )
+
+
 # Plot fecundity data -----------------------------------------------------
 
 # Holistic plot of stocks and years
-fec_data_sum |> 
-  mutate(avg_f_egg_weight = avg_f_egg_weight * 1000) |> # Convert to mg
-  pivot_longer(c(est_fec_corr, avg_f_egg_weight, avg_egg_length)) |> 
-  ggplot(aes(
-    x = poh_length_mm, 
-    y = value, 
-    colour = site)
-    ) +
+(p1 <- fec_data_sum |> 
+   mutate(avg_f_egg_weight = avg_f_egg_weight * 1000) |> # Convert to mg
+   pivot_longer(c(est_fec_corr, avg_f_egg_weight, avg_egg_length)) |> 
+   ggplot(aes(
+     x = poh_length_mm, 
+     y = value, 
+     colour = site)
+   ) +
+   facet_wrap(
+     ~name, 
+     ncol = 1, 
+     strip.position = "left",
+     scales = "free_y",
+     labeller = as_labeller(
+       c(
+         "avg_egg_length" = "Mean egg diameter (mm)",
+         "avg_f_egg_weight" = "Mean egg weight (mg)",
+         "est_fec_corr" = "Estimated fecundity"
+       )
+     )
+   ) +
+   geom_point(
+     aes(shape = year),
+     alpha = 0.5
+   ) +
+   geom_smooth(method = "lm") +
+   stat_poly_eq(
+     formula = y ~ x,
+     aes(label = ..rr.label..),
+     parse = TRUE,
+     label.y = c(0.85, 0.95)) +
+   labs(
+     y = NULL,
+     x = "Post-orbital to hypural length (mm)"
+   ) +
+   theme(strip.placement = "outside",
+         strip.background.y = element_blank())
+)
+
+# Ridgeline plot showing fecundity by age
+(p2 <- fec_data_sum |> 
+  filter(!is.na(resolved_age_gr)) |> 
+  ggplot(aes(est_fec_corr, site)) +
   facet_wrap(
-    ~name, 
-    ncol = 1, 
-    strip.position = "left",
-    scales = "free_y",
-    labeller = as_labeller(
-      c(
-        "avg_egg_length" = "Mean egg diameter (mm)",
-        "avg_f_egg_weight" = "Mean egg weight (mg)",
-        "est_fec_corr" = "Estimated fecundity"
-      )
-    )
-  ) +
-  geom_point(
-    aes(shape = year),
-    alpha = 0.5
+    ~resolved_age_gr,
+    ncol = 1,
+    strip.position = "right",
+    labeller = as_labeller(function(x) (paste("Age", str_extract(x, "[[:digit:]]"))))
     ) +
-  geom_smooth(method = "lm") +
-  labs(y = NULL) +
-  theme(strip.placement = "outside",
-        strip.background.y = element_blank())
+  geom_density_ridges(
+    jittered_points = TRUE,
+    position = "raincloud",
+    alpha = 0.7,
+    scale = 1,
+    point_alpha = 0.7,
+    quantile_lines = TRUE,
+    quantiles = 2,
+    vline_colour = "red"
+  ) +
+  labs(
+    x = "Estimated fecundity",
+    y = "Hatchery stock sampled"
+  )
+)
+
+# Plot showing fecundity by length and age
+fec_data_sum |> 
+  filter(!is.na(resolved_age_gr),
+         !(poh_length_mm > 700 & resolved_age_gr == "31")) |> # Remove some outliers
+  mutate(
+    group = paste0(site, year),
+    resolved_age_gr = str_extract(resolved_age_gr, "^[[:digit:]]")
+  ) |> 
+  group_split(group) |> 
+  map(
+    function(data) {
+      title <- paste(unique(data$site), unique(data$year))
+      
+      plot <- data |> 
+        ggplot(aes(poh_length_mm, est_fec_corr)) +
+        geom_point(aes(colour = resolved_age_gr, size = avg_egg_length),
+                   alpha = 0.75) +
+        geom_smooth(method = "lm", colour = "black") +
+        stat_poly_eq(formula = y~x, 
+                     aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+                     parse = TRUE) + 
+        scale_radius(
+          breaks = c(6:9), 
+          range = c(3, 4.5),
+          guide = guide_legend(
+            title.position = "top",
+            label.position = "bottom",
+            label.vjust = 3
+          )
+        ) +
+        scale_colour_viridis_d(
+          option = "mako", 
+          direction = -1, 
+          end = 0.8,
+          guide = guide_legend(
+            title.position = "top",
+            label.position = "bottom",
+            override.aes = list(size = 4),
+            label.vjust = 3
+          )
+        ) +
+        labs(
+          x = "Post-orbital to hypural length (mm)",
+          y = "Estimated fecundity",
+          size = "Mean egg diameter (mm)",
+          colour = "Total age (years)",
+          title = title
+        ) +
+        theme(legend.position = "bottom")
+      
+      marginal_plot <- ggMarginal(
+        plot,
+        type = "density", 
+        groupColour = TRUE, 
+        groupFill = TRUE,
+        margins = "y"
+      )
+      
+      return(marginal_plot)
+    }
+  )
+
+  
